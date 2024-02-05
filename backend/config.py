68f252e8ea4e915a93e5ee595e73239971b59c6d
frolkin28 <frolkin2801@gmail.com>
2024-02-06 00:30:51 +0200

    init db

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 from backend.utils import get_env, read_yaml
@@ -6,11 +7,20 @@ from backend.utils import get_env, read_yaml
 PROJECT_PATH = Path(__file__).parent.parent
 
 
+@dataclass(slots=True, frozen=True)
 class Config:
-    pass
+    DEBUG: bool
+    SQLALCHEMY_DATABASE_URI: str
 
 
 def get_config() -> Config:
     config_path = PROJECT_PATH / get_env("BACKEND_CONFIG_PATH")
     settings_from_file = read_yaml(config_path)
-    return settings_from_file
+    try:
+        config = Config(
+            DEBUG=settings_from_file["debug"],
+            SQLALCHEMY_DATABASE_URI=settings_from_file["db_uri"],
+        )
+    except KeyError as e:
+        raise RuntimeError("Invalid config format") from e
+    return config
--- backend/utils.py
+++ backend/utils.py
@@ -14,11 +14,3 @@ def read_yaml(file_path: str | Path) -> dict:
     with open(file_path) as file_object:
         data = yaml.load(file_object, Loader=yaml.SafeLoader)
     return data
-
-
-def get_db_connection_string(db_url: str, driver: str | None = None) -> str:
-    driver_formated = "postgresql"
-    if driver:
-        driver_formated = f"postgresql+{driver}"
-
-    return f"{driver_formated}://{db_url}"
