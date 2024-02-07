12fb9482e62a6b43d57c768c91bf0b68935436fc
Oleksandr Bilous <o.bilous712@gmail.com>
2024-02-07 23:10:13 +0200

    Update Lots Creation

--- backend/handlers/lots.py
+++ backend/handlers/lots.py
@@ -4,13 +4,16 @@ from typing import cast
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
 from flask_jwt_extended import get_jwt_identity
-from backend.lib.auth import login_required
+from backend.lib.auth import with_auth_user
 
 from backend.lib.schemas import AddLotSchema
 from backend.types import AddLotPayload
+from backend.models import User
+
 from backend.lib.lots import (
     create_lot
 )
+
 
 log = getLogger(__name__)
 
@@ -18,8 +21,8 @@ bp = Blueprint("lots", __name__, url_prefix="/api/lots")
 
 
 @bp.route("/add_lot", methods=("POST",))
-@login_required
-def add_lot():
+@with_auth_user
+def add_lot(user: User):
     """
     ---
     post:
@@ -41,28 +44,23 @@ def add_lot():
         tags:
         - lots
     """
-
-    user_id = get_jwt_identity
-
+    user_id = user.id
+    
     try:
         request_data = cast(
             AddLotPayload,
             AddLotSchema().load({
-                "lot_name": request.form['lot_name'],
-                "description": request.form['description'],
-                "end_date": request.form['end_date'],
+                "lot_name": request.form.get('lot_name'),
+                "description": request.form.get('description'),
+                "end_date": request.form.get('end_date'),
             })
         )
-
-        if 'file' not in request.files:
-            request_pictures = None
-        else:
-            request_pictures = request.files.getlist('file')
-
-        lot_id = create_lot(request_data, request_pictures, user_id)
-
     except ValidationError as e:
         return jsonify({"errors": e.messages}), 400
+    
+    request_pictures = request.files.getlist('file')
+    lot_id = create_lot(request_data, request_pictures, user_id)
+
     
     response = jsonify({"lot_id": lot_id})
     return response
