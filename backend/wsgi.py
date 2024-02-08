e25256bcc8da0d6c42df852f0ee094f944513632
frolkin28 <frolkin2801@gmail.com>
2024-02-08 02:14:24 +0200

    pretify

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -4,8 +4,8 @@ import logging
 
 from backend.app import create_app
 
-socket, app = create_app()
 
+socket, app = create_app()
 
 gunicorn_logger = logging.getLogger("gunicorn.error")
 app.logger.handlers = gunicorn_logger.handlers
