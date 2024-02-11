4bafbba750ad8d52ca132668167ddede3adee7de
frolkin28 <frolkin2801@gmail.com>
2024-02-11 14:21:04 +0200

    hook

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,6 +1,6 @@
 export { Navbar } from "./Navbar/Navbar";
-export { TextInput } from "./Input/TextInput"
-export { Checkbox } from "./Checkbox/Checkbox"
-export { SignUpForm } from "./Forms/SignUpForm"
-export { SignInForm } from "./Forms/SignInForm"
-export { Button } from "./Buttons/Button"
+export { TextInput } from "./Input/TextInput";
+export { Checkbox } from "./Checkbox/Checkbox";
+export { SignUpForm } from "./Forms/SignUpForm";
+export { SignInForm } from "./Forms/SignInForm";
+export { BetsList } from "./BetsList";
